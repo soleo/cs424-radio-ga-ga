f4ccc9a37ba22f3d4dabbc11f091c6897b3b5fd2
vivekhariharan11@gmail.com <vivekhariharan11@gmail.com@8f27c618-1237-3b13-5af4-164f77b1a4e7>
2011-11-15 03:08:44 +0000

    splitting artists data and loading into database

--- Project4cs424DataProcessing/src/dataprocessing/data/Track.java
+++ Project4cs424DataProcessing/src/dataprocessing/data/Track.java
@@ -4,6 +4,34 @@ public class Track {
 	
 	String trackId;
 	String trackName;
+	String listenedAtUserId;
+	String trackByArtistName;
+	String trackByArtistId;
+	public String getListenedAt() {
+		return listenedAtUserId;
+	}
+	public void setListenedAt(String listenedAt) {
+		this.listenedAtUserId = listenedAt;
+	}
+	public String getTrackBy() {
+		return trackByArtistName;
+	}
+	public void setTrackBy(String trackBy) {
+		this.trackByArtistName = trackBy;
+	}
+	public String getTrackByArtistId() {
+		return trackByArtistId;
+	}
+	public void setTrackByArtistId(String trackByArtistId) {
+		this.trackByArtistId = trackByArtistId;
+	}
+	public String getListenedBy() {
+		return listenedBy;
+	}
+	public void setListenedBy(String listenedBy) {
+		this.listenedBy = listenedBy;
+	}
+	String listenedBy;
 	public String getTrackId() {
 		return trackId;
 	}
