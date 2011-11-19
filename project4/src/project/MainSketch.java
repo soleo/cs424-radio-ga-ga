a2829af3e4cf709d84f302ca0ac0cf8b038d2d0e
jenniferk89@gmail.com <jenniferk89@gmail.com@8f27c618-1237-3b13-5af4-164f77b1a4e7>
2011-11-19 20:34:39 +0000

    added buttons for UI

--- project4/src/project/MainSketch.java
+++ project4/src/project/MainSketch.java
@@ -1,12 +1,23 @@
 package project;
 
+import controlP5.ControlP5;
 import processing.core.*;
 
+
 public class MainSketch extends PApplet {
+	
+Menu theMenu;
 	
 	public void setup(){
 		Utils.globalProcessing = this;
 		Utils.globalProcessing.size(1024, 768);
 		Utils.globalProcessing.smooth();
+		Utils.controlP5 = new ControlP5(this);
+		theMenu = new Menu(200);	
 	}
+	public void draw(){
+		theMenu.drawContent();
+	}
+		
+	
 }
--- project4/src/project/Utils.java
+++ project4/src/project/Utils.java
@@ -1,11 +1,14 @@
 package project;
 
 import processing.core.PApplet;
+import controlP5.*;
 
 //Put all the shared values as static variables here
 
 public class Utils {
 	public static PApplet globalProcessing;
+	public static ControlP5 controlP5;
+
 }
 
 
