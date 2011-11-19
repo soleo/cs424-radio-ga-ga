cd341e949ba850eeb529ee6450e59a9fba2aec2c
kaiserasif@gmail.com <kaiserasif@gmail.com@8f27c618-1237-3b13-5af4-164f77b1a4e7>
2011-11-19 21:21:52 +0000

    countries serialized, not loaded

--- trunk/Project4cs424DataProcessing/src/dataprocessing/processingscripts/CountriesSerializer.java
+++ trunk/Project4cs424DataProcessing/src/dataprocessing/processingscripts/CountriesSerializer.java
@@ -10,31 +10,72 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
+
 public class CountriesSerializer {
+	
 	
 	
 	public static void main(String args[])throws IOException
 	{
 		BufferedReader inputReader=new BufferedReader(new FileReader(new File("outputs/countries")));
-		List<String> countriesList=new ArrayList<String>();
+		//List<String> countriesList=new ArrayList<String>();
+		HashMap<String, Country> countryList = new HashMap<String, Country>();
+		
+		HashMap<String, String> iso = new HashMap<String, String>();
+		BufferedReader cnIsoReader=new BufferedReader(new FileReader(new File("outputs/country.tsv")));
+		while(cnIsoReader.ready())
+		{
+			String inputLine=cnIsoReader.readLine().trim();
+			String[] data = inputLine.split("\t");
+			if(data.length != 2){
+				for(int i=0; i<data.length; i++) 
+					System.out.print(data[i]+" ");
+				System.out.println();
+				//continue;
+			}
+			else
+				iso.put(data[1].trim().toLowerCase(), data[0].trim().toLowerCase());
+		}
+		
+		//for (String key : iso.keySet())
+		//	System.out.println(key + " " + iso.get(key));
+		
 		while(inputReader.ready())
 		{
 			String inputLine=inputReader.readLine().trim();
-			countriesList.add(inputLine);
+			String[] data = inputLine.split("\t");
+			Country c = new Country();
 			
+			c.name = new String(data[0].trim());
+			
+			if(iso.containsKey(c.name))
+			{
+				c.isoCode = new String((String)iso.get(data[0].trim()));
+				c.timeZoneOffset = new String(data[1].trim());
+			
+				//countriesList.add(inputLine);
+				countryList.put(c.name, c);
+			}
+			else
+			{
+				System.out.println(data[0].trim() + "\t" + data[1].trim());
+			}
 			
 		}
 		
-		Collections.sort(countriesList);
+		/*Collections.sort(countriesList);
 		for(int i=0;i<countriesList.size();i++)
 		{
 			System.out.println(countriesList.get(i));
-		}
+		}*/
+		//Collections.sort(countryList);
 		
-		ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(new File("/home/vivek/projects/workspace/visproj4/project4/DataStore/countries.ser")));
-		oos.writeObject(countriesList);
+		//ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(new File("/home/vivek/projects/workspace/visproj4/project4/DataStore/countries.ser")));
+		ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(new File("countries.ser")));
+		oos.writeObject(countryList);
 		oos.close();
 	}
 
---
+++ trunk/Project4cs424DataProcessing/src/dataprocessing/processingscripts/Country.java
@@ -0,0 +1,9 @@
+package dataprocessing.processingscripts;
+
+import java.io.Serializable;
+
+public class Country implements Serializable{
+	public String name;
+	public String isoCode;
+	public String timeZoneOffset;
+}
