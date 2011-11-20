82a439f80e660c2e15f3cf43162b0a62999af3e6
kaiserasif@gmail.com <kaiserasif@gmail.com@8f27c618-1237-3b13-5af4-164f77b1a4e7>
2011-11-20 14:02:34 +0000

    userMap.ser

--- trunk/project4/src/project/MainSketch.java
+++ trunk/project4/src/project/MainSketch.java
@@ -3,12 +3,13 @@ package project;
 import controlP5.ControlP5;
 import processing.core.*;
 import data.*;
+import geomerative.*;
 
 public class MainSketch extends PApplet {
 	
 Menu theMenu;
 	
-	public void setup(){
+	/*public void setup(){
 		Utils.globalProcessing = this;
 		Utils.globalProcessing.size(1024, 768);
 		Utils.globalProcessing.smooth();
@@ -18,7 +19,27 @@ Menu theMenu;
 	}
 	public void draw(){
 		theMenu.drawContent();
-	}
+		
+	}*/
+RShape s;
+RShape polyshp;
+RPoint[] points;
+
+
+public void setup() {
+size(640, 360); 
+RG.init(this);
+s = RG.loadShape("../../BlankMap-World6.svg");
+points = s.getPoints();
+for(int i=0; i<points.length; i++){
+println("(" +points[i].x + ", " + points[i].y +")");
+}
+}
+public void draw() {
+background(255);
+polyshp = RG.polygonize(s);
+RG.shape(polyshp, width/4, 50); 
+}
 		
 	
 }
